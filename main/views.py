8ec5d9ff1e775a06065836eaa5067186c92eb548
nurali-spec <arapovnurali320@gmail.com>
2021-08-14 21:16:41 +0600

    set static and media urls and roots

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
 def homepage(request):
-    return HttpResponse("secont step")
+    return render(request,"todoIndex.html")
 def test(request):
     return render(request,"test.html")
